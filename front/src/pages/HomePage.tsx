c46a95f51031bb93c63c0430a0c92102cc557c63
Hve <hve4638@gmail.com>
2024-10-17 23:51:17 +0900

    REFACTOR : 타입 오류 정정

--- front/src/pages/HomePage.tsx
+++ front/src/pages/HomePage.tsx
@@ -21,11 +21,11 @@ function HomePage() {
         
         const meanings = await LocalInteractive.searchWord(query);
         if (meanings) {
-            const wordData:WordData = {
+            const wordData = {
                 id: -1,
                 word: query,
                 data: meanings,
-            }
+            } as WordData;
 
             eventContext.pushPage(<SearchPage wordData={wordData}/>);
         }
@@ -49,7 +49,7 @@ function HomePage() {
                         className='app-nodrag undraggable fonticon'
                         value='menu'
                         onClick={()=>{
-                            LocalAPI.openBrowser('www.naver.com')
+                            
                         }}
                     />
                 </div>
@@ -108,19 +108,27 @@ function HomePage() {
                                     new QuizGenerator({
                                         onPull(offset:number, limit:number) {
                                             return LocalAPI.getWords(
-                                                [{
-                                                    highFrequencyLimit: 5,
-                                                    lowQuizFrequency: true,
-                                                    shuffle: true,
-                                                    shuffleGroupSize: 5,
-                                                },
+                                                [
+                                                    {
+                                                        // 등장 횟수 10회 이하인 단어 선택
+                                                        // 낮은 등장 빈도 순 정렬 후 5개 단위 셔플
+                                                        highFrequencyLimit: 10,
+                                                        lowQuizFrequency: true,
+                                                        shuffle: true,
+                                                        shuffleGroupSize: 5,
+                                                    },
+                                                    {
+                                                        // 오답률 10% 이상인 단어 선택
+                                                        // 낮은 등장 빈도 & 오답률 높은 순 정렬 후 5개 단위 셔플
+                                                        lowIncorrectRateLimit: 10,
+                                                        lowQuizFrequency: true,
+                                                        highQuizIncorrect: true,
+                                                        shuffle: true,
+                                                        shuffleGroupSize: 5,
+                                                    }
+                                                ],
                                                 {
-                                                    lowQuizFrequency: true,
-                                                    highQuizIncorrect: true,
-                                                    shuffle: true,
-                                                    shuffleGroupSize: 5,
-                                                }],
-                                                {
+                                                    // 두 그룹을 번갈아가며 단어 선택
                                                     order: 'interleave'
                                                 }
                                             );
